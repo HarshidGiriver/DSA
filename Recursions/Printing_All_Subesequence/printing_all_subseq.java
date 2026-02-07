5193921ef04ee06fc8c08557f42251736f4eab9d
HarshidGiriver <harshidgiriver@gmail.com>
2026-02-07 16:51:17 +0530

    added

--- Recursions/Printing_All_Subesequence/printing_all_subseq.java
+++ Recursions/Printing_All_Subesequence/printing_all_subseq.java
@@ -1,5 +1,34 @@
-package Recursions.Printing_All_Subesequence;
+import java.util.*;
+class printing_all_subseq
+{
+ 
+    public static void Print( int index , int arr[] , ArrayList<Integer> curr)
+    {
+            if(arr.length==index)
+            {
+                System.out.println(curr);
+                return;
+            }
 
-public class printing_all_subseq {
-    
+            //Take method
+            curr.add(arr[index]);
+            Print(index+1,arr,curr);
+
+            //backtrack
+            curr.remove(curr.size()-1);
+
+            //Don't take method
+            Print(index+1,arr,curr);
+
+
+    }
+
+    public static void main(String[] args)
+    {
+        int arr[]={1,2,3,4};
+        Print(0,arr,new ArrayList<>());
+
+    }
+ 
+
 }
