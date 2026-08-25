c4a3919599a025998a2dad6511106fa2ef4f4f6c
Harshid Giriver SV <harshidgiriver@gmail.com>
2026-08-25 16:57:31 +0530

    Time: 0 ms (100%), Space: 42.8 MB (44.28%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,7 +1,7 @@
 class Solution {
     public String defangIPaddr(String address) {
-        String s1=address.replace(".","[.]");
-        return s1;
+        
+        return address.replace(".","[.]");
         
     }
 }
